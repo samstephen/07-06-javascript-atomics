8258108210f48fa0915d3db156875603e53fa506
Sam Morgan <samorgan@huskers.unl.edu>
2015-07-06 15:57:46 -0500

    renaming

---
+++ ifstatements/6B/global.js
@@ -0,0 +1,12 @@
+
+
+var password = "monkeybrains";
+
+var userPassword = prompt("What is the password");
+
+if (password === userPassword) {
+  alert("That is correct");
+} else {
+  alert("Sorry, the password is actually " + password);
+
+}
