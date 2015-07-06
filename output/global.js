dfb9b7927f8d3c067159cc25ce906bd969e56e34
Sam Stephen <samueljstephen@gmail.com>
2015-07-06 11:26:23 -0500

    output exercise - alert will pop up as user leaves/reloads

---
+++ output/global.js
@@ -0,0 +1,28 @@
+/**
+ * Created by samuelstephen on 7/6/15.
+ */
+
+// Create an alert() that greets the visitor and tells them your name
+alert("Hi, I'm Sam.");
+
+// Use console.log() to output (one console.log() statement per line)
+console.log("Your favorite breakfast?");
+console.log("How many cats live with you");
+console.log("Your favorite place to eat lots of food");
+console.log("The number of naps you take per week.");
+console.log("The number of naps you'd like to take per week");
+
+// Another alert() that says "Goodbye" to the user.
+window.onbeforeunload = alert;
+function alert()
+{
+    return "Goodbye!";
+}
+
+
+
+
+
+document.write("<p>document.write will write to the body of a webpage</p>");
+
+
