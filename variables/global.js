e05ad38d59084ca2eae675ae59ec95383decf771
Sam Stephen <samueljstephen@gmail.com>
2015-07-06 12:16:01 -0500

    variables exercise - experiment with variable in alert/console/document

---
+++ variables/global.js
@@ -0,0 +1,42 @@
+/**
+ * Created by samuelstephen on 7/6/15.
+ */
+
+
+// Create three variables: name, age, and hairColor
+
+var name;
+var age;
+var hairColor;
+
+// Assign to each of those variables some reasonable value (for example, my name is "Andrew", my age is 29, and my hairColor is "Bright Pink") -- I don't care if you use your own information, that of a fictional character (or a cat!), or some mix thereof. age should be an integer (like 29, not "29"); the other variables should be strings.
+name = "Sam";
+age = 24;
+hairColor = "brown";
+
+// Do three alert() statements, with output formatted like so:
+// My name is Andrew.
+alert("My name is " + name + ".");
+
+// My age is 29.
+alert("My age is " + age + ".");
+// My hair color is Bright Pink.
+alert("My hair color is " + hairColor + ".")
+// (Note how the values for each of these match the values I provided in step 2 exactly. Also: pay attention to whitespace and formatting).
+
+// Make sure that the output statements you create in Step 3 include the relevant variables, rather than duplicating the information stored in the variables.
+
+// Once you've gotten it working, change the values you assign to your variables (don't change the output statements at all), and see how your program changes as you run it. See what happens if you use console.log() instead of alert(). See what happens if you change age to be a string instead of an integer (e.g. to "29" instead of 29).
+name = "Patrick";
+age = "19";
+hairColor = "light-brown";
+
+console.log("My name is " + name + ".");
+console.log("My age is " + age + ".");
+console.log("My hair color is " + hairColor + ".");
+
+
+var title;
+
+document.write(title);
+console.log(title);
