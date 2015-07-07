a428e9956fd63970002380cdd555fe79b43808c6
Patrick Carlton <pcarlton73@ggmail.com>
2015-07-07 14:51:00 -0500

    7H changed while to for.

---
+++ basicloops/7H/global.js
@@ -0,0 +1,31 @@
+var count = 1;
+var start = prompt("enter a num, dirtbag!")
+var end = prompt("do it again, chicken wang!")
+
+
+
+// while (start <= end) {
+//   if (start % 3 == 0 && start % 5 == 0) {
+//     console.log("fizzbuzz");
+//   } else if (start % 3 == 0) {
+//     console.log("fizz");
+//   } else if (start % 5 == 0) {
+//     console.log("buzz");
+//   } else {
+//     console.log(start);
+//   }
+//   start++;
+// }
+
+
+for (start; start <= end; start++) {
+  if (start % 3 == 0 && start % 5 == 0) {
+    console.log("fizzbuzz");
+  } else if (start % 3 == 0) {
+    console.log("fizz");
+  } else if (start % 5 == 0) {
+    console.log("buzz");
+  } else {
+    console.log(start);
+  }
+}
