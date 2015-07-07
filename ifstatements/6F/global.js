6ea8d749cae7922114b818fa376fffe9a0e6906b
Sam Stephen <samueljstephen@gmail.com>
2015-07-07 11:47:13 -0500

    6f triangles program start

--- ifstatements/6F/global.js
+++ ifstatements/6F/global.js
@@ -1,27 +1,36 @@
-console.log( "5 == '5' is " + (5 == '5') ); //true
-console.log( "5 == 'five' is " + (5 == 'five') );
-console.log( "5 == '6' is " + (5 == '6') );
-console.log( "5 === '5' is " + (5 === '5') );
-console.log( "5 == 2 + 3 is " + (5 == 2 + 3) );
-console.log( "5 == 5 is " + (5 == 5) );
-console.log( "5 === 5 is " + (5 === 5) );
-console.log( "true == 'true' is " + (true == 'true') );
-console.log( "true === 'true' is " + (true === 'true') );
-console.log( "true == false is " + (true == false) );
-console.log( "5 == 12 is " + (5 == 12) );
-console.log( "5 !== 12 is " + (5 != 12) );
-console.log( "5 < 12 is " + (5 < 12) );
-console.log( "5 <= 12 is " + (5 <= 12) );
-console.log( "5 > 12 is " + (5 > 12));
-console.log( "5 >= 12 is " + (5 >= 12));
+/*
+Write a program which prompts your user for the lengths of the 3 legs of a triangle
+(this input can be an integer or a floating point number, but it can't be a string),
+and which then reports whether this is a valid triangle. 
+It's probably worthwhile to output the results of your behind-the-scenes calculations to the console,
+ but the ultimate output ("This is a valid triangle" or "This is not a valid triangle" should be handled with an alert).
+*/
 
-var password = "monkeybrains";
+var valid = "This is a valid triangle";
+var invalid = "This is not a valid triangle";
 
-var userPassword = prompt("What is the password");
+var firstLeg = prompt("Make a triangle, Enter First leg's length: ");
+firstLeg = parseFloat(firstLeg);
+console.log(firstLeg);
 
-if (password === userPassword) {
-  alert("That is correct");
+var secondLeg = prompt("Enter Second leg's length: ");
+secondLeg = parseFloat(secondLeg);
+console.log(secondLeg);
+
+var thirdLeg = prompt("Enter Third leg's length: ");
+thirdLeg = parseFloat(thirdLeg);
+console.log(thirdLeg);
+
+
+/*
+For all triangles, the sum of the lengths of any two sides must be greater than the length of the third side. 
+For example: a triangle of 3, 4, 5 (in whatever units you want) is valid (since 3 + 4 > 5, 3 + 5 > 4, and 4 + 5 > 3),
+whereas a triangle of 3, 4, 7 is not (since 3 + 4 is not greater than 7).
+*/
+
+if (thirdLeg < (firstLeg + secondLeg) || firstLeg < (thirdLeg + secondLeg) || secondLeg < (thirdLeg + firstLeg)) {
+	alert(valid);
 } else {
-  alert("Sorry, the password is actually " + password);
-
+	alert(invalid);
 }
+
